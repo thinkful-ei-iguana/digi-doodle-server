11d332fd1250c09bfbcd61abc6df983140231fb6
Michael Weedman <michael.weedman@gmail.com>
2020-02-16 10:59:51 -0700

    adding socket locally to play with.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const app = require('./app');
 const knex = require('knex');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 const { PORT, DATABASE_URL } = require('./config');
 
@@ -12,7 +14,13 @@ const db = knex({
 
 app.set('db', db);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log('Database URL:', DATABASE_URL);
   console.log(`Server listening at http://localhost:${PORT}`);
 });
+
+var chat = io
+  .of('/game')
+  .on('connection', (socket) => {
+    socket.emit('lookie here!');
+  });
